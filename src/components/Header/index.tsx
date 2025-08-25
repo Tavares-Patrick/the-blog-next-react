e86ca8a8d20019ded4ab336fde67a6e6529c9fc8
Patrick Tavares <patrick.t2@hotmail.com>
2025-08-25 11:02:59 -0300

    tailwind

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import clsx from "clsx";
+
+export function Header() {
+  return (
+    <h1
+        className={clsx(
+          'text-xl',
+          'font-bold',
+          'text-blue-500',
+          'hover:text-blue-50',
+          'hover:bg-blue-500',
+          'transition',
+          'duration-1000'
+          )}
+        >
+          Texto no meu h1
+        </h1>
+  )
+}
