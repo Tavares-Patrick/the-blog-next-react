395368b3a32c6b2d951389029e40ee72f326aff9
Patrick Tavares <patrick.t2@hotmail.com>
2025-09-04 23:04:33 -0300

    PostHeading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import Link from "next/link";
 import Image from "next/image";
+import { PostHeading } from "@/components/PostHeading";
 
 
 export default async function HomePage() {
@@ -16,13 +17,20 @@ export default async function HomePage() {
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
         <Link className="w-full h-full overflow-hidden rounded-xl" href={"#"}>
           <Image
-            className="group-hover:scale-105 transition"
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
             src='/img/bryen_0.png'
             width={1200} height={720}
             alt="Título do post"
+            priority
           />
         </Link>
-        <div>
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time className="text-slate-600 block text-sm/tight" dateTime="2025-09-04">04/06/2025 10:00</time>
+
+          <PostHeading as='h1' url='#'>
+            Type and scrambled it to make a type
+          </PostHeading>
+
           Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           Lorem Ipsum has been the standard dummy text ever since the 1500s, when an
           unknown printer took a galley of type and scrambled it to make a type
